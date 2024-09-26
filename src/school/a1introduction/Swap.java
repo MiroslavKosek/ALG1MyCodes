df141845c7a7d5887aaf117381e6ac6e16d3418f
m.kosek <m.kosek@netsystem.cz>
2024-09-26 11:47:36 +0200

    added Swap.java

--- src/school/a1introduction/Swap.java
+++ src/school/a1introduction/Swap.java
@@ -6,6 +6,29 @@ package school.a1introduction;
  */
 public class Swap {
     public static void main(String[] args) {
-
+        //input
+        int a = 34;
+        int b = 89;
+        
+        System.out.println("Puvodni hodnoty:");
+        System.out.println("a = " + a + ", b = " + b);
+        
+        //1. Bez vymeny v pameti:
+        System.out.println("1. Bez vymeny v pameti:");
+        System.out.println("a = " + b + ", b = " + a);
+        
+        //2. S vymenou v pameti s pomocnou promennou:
+        System.out.println("2. S vymenou v pameti s pomocnou promennou:");
+        int temp = a;
+        a = b;
+        b = temp;
+        System.out.println("a = " + a + ", b = " + b);
+        
+        //3. S vymenou v pameti bez pomocne promenne
+        System.out.println("3. S vymenou v pameti bez pomocne promenne:");
+        a = a + b;
+        b = a - b;
+        a = a - b;
+        System.out.println("a = " + a + ", b = " + b);
     }
 }
