cddefbe58560357bcfdfa2ec4e6a6503c24c4b86
m.kosek <m.kosek@netsystem.cz>
2024-10-24 12:06:41 +0200

    updated U3235

--- src/school/a3branchingloops/U3235For.java
+++ src/school/a3branchingloops/U3235For.java
@@ -8,30 +8,49 @@ public class U3235For {
     
     public static void main(String[] args) {
         int n = 5;
+        int m = 6;
         
         DrawLine(n);
         
         System.out.println("");
         
-        int m = 6;
+        DrawRectangle(m, n);
+        
+        System.out.println("");
+        
+        DrawRightAngledTriangle(n);
+        
+        System.out.println("");
+        
+        DrawTriangle(n);
+    }
+    
+    public static void DrawLine(int n) {
+        for (int i = 1; i <= n; i++) {
+            System.out.print("* ");
+        }
+        System.out.println("");
+    }
+    
+    public static void DrawRectangle(int m, int n) {
         for (int i = 1; i <= m; i++) {
             for (int j = 1; j <= n; j++) {
                 System.out.print("* ");
             }
             System.out.println("");
         }
-        
-        System.out.println("");
-        
+    }
+    
+    public static void DrawRightAngledTriangle(int n) {
         for (int i = 1; i <= n; i++) {
             for (int j = 1; j <= i; j++) {
                 System.out.print("* ");
             }
             System.out.println("");
         }
-        
-        System.out.println("");
-        
+    }
+    
+    public static void DrawTriangle(int n) {
         for (int i = 1; i <= n; i++) {
             for (int j = 1; j <= n - i; j++) {
                 System.out.print(" ");
@@ -42,12 +61,4 @@ public class U3235For {
             System.out.println("");
         }
     }
-    
-    public static void DrawLine(int n) {
-        for (int i = 1; i <= n; i++) {
-            System.out.print("* ");
-        }
-        System.out.println("");
-    }
-    
 }
